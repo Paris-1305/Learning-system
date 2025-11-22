6536009f2f409167d9ad3cd2958be9d45eeaed6b
Paris-1305 <boyomnzussouodaringsteve@gmail.com>
2025-11-22 18:52:53 +0100

    try

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ProAAvider>
+    </Provider>
   </React.StrictMode>
 );
 
